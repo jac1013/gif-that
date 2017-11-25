e1d49ab1264227b715fcacab6e4771af2d2adedc
Joseph Arrieta <jac.wshmstr@gmail.com>
2017-11-25 11:54:46 -0500

    for now let's run the tests in this branch, later we add the exception to run them just in the master branch

--- tests.sh
+++ tests.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 npm run test-ci
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-  npm run start-production &
-  sleep 15
-  npm run test-end2end-browserstack-ci
-fi
+npm run start-production &
+sleep 15
+npm run test-end2end-browserstack-ci
